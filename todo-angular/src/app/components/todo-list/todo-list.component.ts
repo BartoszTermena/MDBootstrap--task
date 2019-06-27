4e306ce48abd11213da1883f52711afdc8b5c119
unknown <bartosz.termena@gmail.com>
2019-06-27 09:56:13 +0200

    Completed todo-angular

--- todo-angular/src/app/components/todo-list/todo-list.component.ts
+++ todo-angular/src/app/components/todo-list/todo-list.component.ts
@@ -12,22 +12,37 @@ export class TodoListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.todoId = 1;
-    this.todos = [];
+    this.todos = this.getTodos();
+    if(this.todos.length == 0) {
+      this.todoId = 1
+    } else {
+      this.todoId = this.todos.length + 1;
+    }
+  }
+  public getTodos(): Object[] {
+    let localStorageItem = JSON.parse(localStorage.getItem('todos'));
+    return localStorageItem == null ? [] : localStorageItem.todos;
   }
   addTodo(): void {
     this.todos.push({
       id: this.todoId,
       title: `Task number ${this.todoId}`
     })
+    this.setLocalStorageTodos(this.todos)
     this.todoId+=1;
   }
   deleteLastTodo(): void {
     this.todos.pop();
+    this.setLocalStorageTodos(this.todos)
     this.todoId-=1;
   }
   clearTodos(): void {
     this.todos = [];
+    this.setLocalStorageTodos(this.todos)
     this.todoId = 1;
+  }
+
+  private setLocalStorageTodos(todos: Object[]): void {
+    localStorage.setItem('todos', JSON.stringify({todos}))
   }
 }
